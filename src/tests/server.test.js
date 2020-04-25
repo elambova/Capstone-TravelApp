d77a8b7c5419b7ad3ccc3003f779bc703b79b811
Elena Lambova <elena.g.lambova@gmail.com>
2020-04-26 01:19:37 +0300

    Refactor: Remove one of tests

--- src/tests/server.test.js
+++ src/tests/server.test.js
@@ -2,15 +2,6 @@ const request = require("supertest");
 const app = require("../server/server");
 
 describe("api", () => {
-  describe("get /all", () => {
-    it("should return a 200", () => {
-      request(app)
-        .get("/all")
-        .then((res) => {
-          expect(res.statusCode).toBe(200);
-        });
-    });
-  });
   describe("get /getInfo", () => {
     test("should return a 200", async () => {
       const response = await request(app).get("/getInfo");
