4796265b8486d8e6219593c04d8e5c27cd6a16a7
Elena Lambova <elena.g.lambova@gmail.com>
2020-04-25 23:45:35 +0300

    Add all config files

---
+++ webpack.prod.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+// const WorkboxPlugin = require("workbox-webpack-plugin");
+
+module.exports = {
+  entry: "./src/client/index.js",
+  mode: "production",
+  output: {
+    libraryTarget: "var",
+    library: "Client",
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+      favicon: "./src/client/media/favicon.ico",
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/todoList.html",
+      filename: "./todoList.html",
+      favicon: "./src/client/media/favicon.ico",
+    }),
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
+    // new WorkboxPlugin.GenerateSW(),
+  ],
+  node: {
+    fs: "empty",
+  },
+};
