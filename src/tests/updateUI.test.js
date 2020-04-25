614340ccc0684a4bf4b4220e91387ee22f393a27
Elena Lambova <elena.g.lambova@gmail.com>
2020-04-26 00:32:51 +0300

    Tests: Add test for client/js/updateUI.js file

---
+++ src/tests/updateUI.test.js
@@ -0,0 +1,13 @@
+import { updateUI } from "../client/js/updateUI";
+
+describe("updateUI", () => {
+  test("updateUI should be defined", () => {
+    expect(updateUI).toBeDefined();
+  });
+});
+
+describe("updateUI", () => {
+  test("updateUI should be a function", () => {
+    expect(typeof updateUI).toBe("function");
+  });
+});
