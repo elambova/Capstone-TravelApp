3f546cde6fad594f11c4c63c903f960b7b9c86b5
Elena Lambova <elena.g.lambova@gmail.com>
2020-04-26 00:39:50 +0300

    Refactor: Add loading image.

--- src/client/index.js
+++ src/client/index.js
@@ -6,6 +6,13 @@ import { updateUI } from "./js/updateUI";
 import "./styles/reset.scss";
 import "./styles/style.scss";
 
+// import image/gif file
+import loading from "./media/loading.gif";
+
+// get image from html and set source
+const loadingGif = document.getElementById("loading");
+loadingGif.src = loading;
+
 // Add event listener to form travel. When button is clicked callback function destinationInfo will execute
 document.getElementById("travel").addEventListener("submit", (e) => {
   e.preventDefault();
