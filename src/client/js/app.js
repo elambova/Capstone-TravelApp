b97a980e8ba0712c6c56caf9c95d4956ad686ed4
Elena Lambova <elena.g.lambova@gmail.com>
2020-07-21 10:37:01 +0300

    Feat: Add Local Storage functionality to safe trip information, and show when page in open again.

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -4,6 +4,12 @@ const city = document.getElementById("city");
 const dateStart = document.getElementById("date-start");
 const dateEnd = document.getElementById("date-end");
 
+let destinationsArray = localStorage.getItem("destination")
+  ? JSON.parse(localStorage.getItem("destination"))
+  : [];
+
+const dataStorage = JSON.parse(localStorage.getItem("destination"));
+
 const localhost = "http://localhost:8081/";
 
 // postData function using the keyword async and use method POST to post data which we will need later
@@ -25,12 +31,24 @@ const postData = async (url = "", data = {}) => {
   }
 };
 
-// destinationInfo function collect functions above starting from getData and wiht Promises (and key word then) add postData i updateUI.
+// destinationInfo function collect functions above starting from getData and wiht Promises (and key word then) add postData and updateUI.
 function destinationInfo() {
   // loading image
   document.getElementById("loading").style.display = "block";
-
-  if (city.value.trim().length !== 0) {
+  if (city.value.trim().length === 0 && dataStorage.length != 0) {
+    fetch(
+      `${localhost}getInfo?city=${city.value}&start=${dateStart.value}&end=${dateEnd.value}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        postData(`${localhost}saveData`, data);
+        console.log(dataStorage);
+        Client.updateUI(dataStorage);
+      })
+      .catch((err) => console.error(err));
+  } else if (city.value.trim().length !== 0) {
     city.hasAttribute("class") ? city.removeAttribute("class") : null;
     fetch(
       `${localhost}getInfo?city=${city.value}&start=${dateStart.value}&end=${dateEnd.value}`
@@ -40,9 +58,9 @@ function destinationInfo() {
       })
       .then((data) => {
         postData(`${localhost}saveData`, data);
+        localStorage.setItem("destination", JSON.stringify(data));
         Client.updateUI(data);
       })
-      // .then()
       .catch((err) => console.error(err));
   } else {
     city.classList.add("error");
