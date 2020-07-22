1a7c453b10018654401aebfeeeded71267be0629
Elena Lambova <elena.g.lambova@gmail.com>
2020-07-22 12:30:07 +0300

    Feat: Add new function.

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -1,100 +1,103 @@
+const ui = (data) => {
+  // loading image hide if data is resive
+  document.getElementById("loading").style.display = "none";
+
+  // set display flex to .box holder to show all data
+  document.getElementsByClassName("box")[0].style.display = "flex";
+
+  // document.getElementById("add-destination").style.display = "block";
+
+  //This adds the name  to the results span
+  document.getElementById("city-result").innerHTML = data.city;
+
+  //This adds the name  to the results span
+  document.getElementById("country").innerHTML = data.country;
+
+  //This adds the name  to the results span
+  document.getElementById("arrival-date-result").innerHTML = data.start;
+
+  //This adds the name  to the results span
+  document.getElementById("departure-date-result").innerHTML = data.end;
+
+  //This adds the temp to the results span
+  document.getElementById("long").innerHTML = data.long;
+
+  // This is adding the result data to the span
+  document.getElementById("lat").innerHTML = data.lat;
+
+  //This is getting the number of days until the vacation
+  const daysAway = data.daysUntilTrip;
+
+  // This is adding the result data to the span
+  daysAway <= 1
+    ? (document.getElementById("days-away").innerHTML = daysAway + " day away!")
+    : (document.getElementById("days-away").innerHTML =
+        daysAway + " days away!");
+
+  //This is getting the number of days until total for the vacation
+  const daysTotal = data.tripLength;
+
+  // This is adding the result data to the span
+  daysTotal <= 1
+    ? (document.getElementById("trip-length").innerHTML = daysTotal + " day.")
+    : (document.getElementById("trip-length").innerHTML = daysTotal + " days.");
+
+  // This is adding the result data to the img
+  document
+    .getElementById("forecast-icon")
+    .setAttribute(
+      "src",
+      `https://www.weatherbit.io/static/img/icons/${data[0].weather.icon}.png`
+    );
+
+  // This is adding the result data to the span
+  document
+    .getElementById("forecast-icon")
+    .setAttribute("alt", `${data[0].weather.description}`);
+
+  // This is adding the result data to the span
+  document.getElementById("forecast").innerHTML = data[0].weather.description;
+
+  // This is adding the result data to the span
+  document.getElementById("high-temp").innerHTML =
+    data[0].high_temp + " \xB0C.";
+
+  // This is adding the result data to the span
+  document.getElementById("low-temp").innerHTML = data[0].low_temp + " \xB0C.";
+
+  // This is adding the result data to the image src
+  document
+    .getElementById("travel-destination")
+    .setAttribute("src", data.picture);
+
+  // This is adding the result data to the image alt
+  document.getElementById("travel-destination").setAttribute("alt", data.city);
+
+  // This is adding the result data to the p
+  document.getElementById("tags").innerHTML = data.tags;
+
+  const multipleDays = document.getElementById("temp-for-multiple-days");
+
+  const total = parseInt(daysAway) + parseInt(daysTotal);
+
+  if (total <= 16) {
+    if (multipleDays.hasChildNodes()) {
+      multipleDays.innerHTML = "";
+      createElementMultipleWeather(multipleDays, data, total);
+    } else {
+      createElementMultipleWeather(multipleDays, data, total);
+    }
+  }
+};
+
 // updateUI function using the keyword async and updates the allData we have, and attach to selected elements
 const updateUI = async (data) => {
+  if (Array.isArray(data)) {
+    ui(data[0]);
+  } else {
+    ui(data);
+  }
   try {
-    // loading image hide if data is resive
-    document.getElementById("loading").style.display = "none";
-
-    // set display flex to .box holder to show all data
-    document.getElementsByClassName("box")[0].style.display = "flex";
-
-    // document.getElementById("add-destination").style.display = "block";
-
-    //This adds the name  to the results span
-    document.getElementById("city-result").innerHTML = data.city;
-
-    //This adds the name  to the results span
-    document.getElementById("country").innerHTML = data.country;
-
-    //This adds the name  to the results span
-    document.getElementById("arrival-date-result").innerHTML = data.start;
-
-    //This adds the name  to the results span
-    document.getElementById("departure-date-result").innerHTML = data.end;
-
-    //This adds the temp to the results span
-    document.getElementById("long").innerHTML = data.long;
-
-    // This is adding the result data to the span
-    document.getElementById("lat").innerHTML = data.lat;
-
-    //This is getting the number of days until the vacation
-    const daysAway = data.daysUntilTrip;
-
-    // This is adding the result data to the span
-    daysAway <= 1
-      ? (document.getElementById("days-away").innerHTML =
-          daysAway + " day away!")
-      : (document.getElementById("days-away").innerHTML =
-          daysAway + " days away!");
-
-    //This is getting the number of days until total for the vacation
-    const daysTotal = data.tripLength;
-
-    // This is adding the result data to the span
-    daysTotal <= 1
-      ? (document.getElementById("trip-length").innerHTML = daysTotal + " day.")
-      : (document.getElementById("trip-length").innerHTML =
-          daysTotal + " days.");
-
-    // This is adding the result data to the img
-    document
-      .getElementById("forecast-icon")
-      .setAttribute(
-        "src",
-        `https://www.weatherbit.io/static/img/icons/${data[0].weather.icon}.png`
-      );
-
-    // This is adding the result data to the span
-    document
-      .getElementById("forecast-icon")
-      .setAttribute("alt", `${data[0].weather.description}`);
-
-    // This is adding the result data to the span
-    document.getElementById("forecast").innerHTML = data[0].weather.description;
-
-    // This is adding the result data to the span
-    document.getElementById("high-temp").innerHTML =
-      data[0].high_temp + " \xB0C.";
-
-    // This is adding the result data to the span
-    document.getElementById("low-temp").innerHTML =
-      data[0].low_temp + " \xB0C.";
-
-    // This is adding the result data to the image src
-    document
-      .getElementById("travel-destination")
-      .setAttribute("src", data.picture);
-
-    // This is adding the result data to the image alt
-    document
-      .getElementById("travel-destination")
-      .setAttribute("alt", data.city);
-
-    // This is adding the result data to the p
-    document.getElementById("tags").innerHTML = data.tags;
-
-    const multipleDays = document.getElementById("temp-for-multiple-days");
-
-    const total = parseInt(daysAway) + parseInt(daysTotal);
-
-    if (total <= 16) {
-      if (multipleDays.hasChildNodes()) {
-        multipleDays.innerHTML = "";
-        createElementMultipleWeather(multipleDays, data, total);
-      } else {
-        createElementMultipleWeather(multipleDays, data, total);
-      }
-    }
   } catch (error) {
     return error;
   }
